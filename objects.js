9757d8a77b649d73aff63ab90e859d6f057ad3fc
Robert Daub <rjd2066@gmail.com>
2020-04-07 03:28:27 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,6 +4,6 @@ var playlist = {
 }
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist[artistName] = songTitle
+  playlist['Slowdive', 'My Bloody Valentine', 'Phil Ochs'] = 'Alison', 'Soon', 'Changes'
   return playlist
 }
