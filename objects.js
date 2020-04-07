64fb57e9a58c30eb1553564138d47ed2012ad115
Robert Daub <rjd2066@gmail.com>
2020-04-07 01:52:04 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,6 +4,10 @@ var playlist = {
 }
 
 function updatePlaylist(playlist, artist, song) {
-  playlist[artist] = song
+  playlist['Nirvana'] = 'Aneurysm'
   return playlist
+}
+
+function removeFromPlaylist(playlist, artist) {
+  delete playlist[]
 }
