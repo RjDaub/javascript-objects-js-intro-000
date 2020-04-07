28e37a0448b9f8b8b624d69277a59e1a76be6e91
Robert Daub <rjd2066@gmail.com>
2020-04-07 02:28:24 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -5,6 +5,8 @@ var playlist = {
 
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist = {
+    'Foo Fighters': 'Everlong',
+    'Prince': 'Purple Rain'
     'Slowdive': 'My Bloody Valentine'
   }
     return playlist
