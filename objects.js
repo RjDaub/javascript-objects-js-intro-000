2c194a6a128217f80dc4270a8d98bb55d0a39a51
Robert Daub <rjd2066@gmail.com>
2020-04-07 01:49:48 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,3 +2,10 @@ var playlist = {
   'Foo Fighters': 'Everlong',
   Prince: 'Purple Rain'
 }
+
+function updatePlaylist(playlist, artist, song) {
+  playlist[artist] = song  
+  return playlist
+}
+
+
